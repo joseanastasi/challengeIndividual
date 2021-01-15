64ecb2d96c7da729bb6fe47a6a698dbe397baebe
josetango <josetango18@gmail.com>
2021-01-15 14:32:03 -0300

    Test

--- src/main/java/com/example/demo/interfaces/IBudget.java
+++ src/main/java/com/example/demo/interfaces/IBudget.java
@@ -9,14 +9,13 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.modelo.Budget;
 
 @Repository
-//TODO: cambiar nombre clase a BudgetRepository
+
 public interface IBudget extends JpaRepository<Budget, Integer> {
 	
 	@Query(value = "SELECT sum(amount) FROM budget", nativeQuery = true)
-	//TODO: en vez que String tiene que ser Float
 	String sum();
 	
 	@Query(value = "SELECT * FROM budget order by date LIMIT 10;", nativeQuery = true)
-	//TODO: debe ser lastTen
+	
 	List<Budget> lasTen();
 }
--- src/test/java/com/example/demo/controller/BudgetControllerTest.java
+++ src/test/java/com/example/demo/controller/BudgetControllerTest.java
@@ -5,11 +5,13 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.Before;
@@ -17,6 +19,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.context.ContextConfiguration;
@@ -24,6 +27,7 @@ import org.springframework.test.context.TestContext;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.example.demo.controler.BudgetController;
@@ -39,6 +43,9 @@ public class BudgetControllerTest {
 	@Mock
 	private IBudgetService budgetServiceMock;
 
+	@Mock
+	private BindingResult mockBindingResult;
+	
 	@InjectMocks
 	private BudgetController budgetController;
 
@@ -47,63 +54,38 @@ public class BudgetControllerTest {
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
+	    Mockito.when(mockBindingResult.hasErrors()).thenReturn(false);
 		mockMvc = MockMvcBuilders.standaloneSetup(budgetController).build();
 	}
 	
-//	@Test
-//	public void test_deleteBudgetEntryFound() throws Exception {
-//
-//
-//		Budget budget = new Budget();
-//		Long noTxn = 1L;  
-//		String conceptMock = "Junit and Mockito";
-//		Float amountMock = 15.23F;
-//		Date dateMock = new Date(2020-05-05);
-//		
-//		budget.setDate(dateMock);
-//		budget.setConcept(conceptMock);
-//		budget.setNoTxn(noTxn);
-//		budget.setAmount(amountMock);
-//		
-//		when(newsServiceMock.getBudgetById(noTxn)).thenReturn(budget);
-//
-//		mockMvc.perform(get("/delete/{noTxn}", 1L)).andExpect(status().is3xxRedirection())
-//				.andExpect(view().name("redirect:/admin/news"));
-//
-//		ArgumentCaptor<Long> formObjectArgument = ArgumentCaptor.forClass(Long.class);
-//		verify(newsServiceMock).deleteNewsById(formObjectArgument.capture());
-//
-//		Long formObject = formObjectArgument.getValue();
-//
-//		assertNotNull(formObject);
-//		assertTrue(formObject.equals(1L));
-//
-//		verify(newsServiceMock, times(1)).deleteNewsById(1L);
-//		verify(newsServiceMock, times(1)).getNewsById(1L);
-//		verifyNoMoreInteractions(newsServiceMock);
-//	}
+	
+	@Test
+	public void test_listBudgetEntryFound() throws Exception { 
+		Budget budget1 = createBudget();
+    	Budget budget2 = createBudget();
+    	List<Budget> budgetList = List.of(budget1, budget2);
+    	String s = "5";
+	
+		when(budgetServiceMock.lastTen()).thenReturn(budgetList);	
+		when(budgetServiceMock.sum()).thenReturn(s);	
 
-//	@GetMapping("/delete/{noTxn}")
-//	public String delete(@PathVariable int noTxn, Model model) {
-//		service.delete(noTxn);
-//		return "redirect:/list";
-//	}	
+		mockMvc.perform(get("/list")).andExpect(status().isOk())
+				.andExpect(view().name("index"))
+				.andExpect(model().attribute("budgets", budgetList))
+				.andExpect(model().attribute("suma", s));
+
+		verify(budgetServiceMock, times(1)).lastTen();
+		verify(budgetServiceMock, times(1)).sum();
+
+		verifyNoMoreInteractions(budgetServiceMock);
+	}
 		
 
 	
 	@Test
 	public void test_editBudgetEntryFound() throws Exception { 
-		Budget budget = new Budget();
+		Budget budget = createBudget();
 		int noTxn = 1;  
-		String conceptMock = "Junit and Mockito";
-		Float amountMock = 15.23F;
-		Date dateMock = new Date(2020-05-05);
-		
-		budget.setDate(dateMock);
-		budget.setConcept(conceptMock);
-		budget.setNoTxn(noTxn);
-		budget.setAmount(amountMock);
-				
 	
 		when(budgetServiceMock.listNoTxn(noTxn)).thenReturn(Optional.of(budget));	
 		
@@ -117,6 +99,29 @@ public class BudgetControllerTest {
 		verify(budgetServiceMock, times(1)).listNoTxn(1);
 		verifyNoMoreInteractions(budgetServiceMock);
 	}
-		
-
+	
+	@Test
+	public void test_addBudgetEntryFound() throws Exception { 
+		Budget budget = createBudget();
+		  		
+		mockMvc.perform(get("/add")).andExpect(status().isOk())
+				.andExpect(view().name("form"));
+		}
+	
+	
+	 private Budget createBudget(){
+	    	Budget budget = new Budget();
+			
+	    	int noTxn = 1;  
+			String conceptMock = "Junit and Mockito";
+			Float amountMock = 15.23F;
+			Date dateMock = new Date(2020-05-05);
+			
+			budget.setDate(dateMock);
+			budget.setConcept(conceptMock);
+			budget.setNoTxn(noTxn);
+			budget.setType("Egreso");
+			budget.setAmount(amountMock);
+			return budget;
+	    }    	
 }
